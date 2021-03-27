06929463671c860ac71232a0926a863bb5f74499
王龙浚 <longxun.wang@tcl.com>
2021-03-27 16:25:03 +0800

    计数排序

---
+++ src/top/lpepsi/JikeCount.java
@@ -0,0 +1,54 @@
+package top.lpepsi;
+
+import java.util.Arrays;
+
+/**
+ * @author 林北
+ * @description 计数排序
+ * @date 2021-03-27 15:59
+ */
+public class JikeCount {
+    /**
+     * @Author 林北
+     * @Description //TODO 计数排序
+     * @Param [nums, n]
+     * @return void
+     **/
+
+     public static void countingSort(int[] nums ,int n){
+         if (n <= 0){
+             return;
+         }
+         int max = nums[0];
+         for (int i = 0; i < n; i++){
+             if (max < nums[i]){
+                 max = nums[i];
+             }
+         }
+         int[] b = new int[max+1];
+         for (int i = 0; i < n; i++) {
+             b[nums[i]]++;
+         }
+         for (int i = 1; i < b.length; i++) {
+             b[i] = b[i-1] +b[i];
+         }
+         int[] temp = new int[n];
+         for (int i = nums.length - 1; i >= 0; i--) {
+             int params = nums[i];
+             int index = b[params];
+             temp[index-1] = params;
+             b[params]--;
+         }
+         System.arraycopy(temp, 0, nums, 0, temp.length - 1 + 1);
+     }
+
+    public static void main(String[] args) {
+        int[] nums = {4,32,1,6,2,5,9,2,0};
+        System.out.println("排序前:");
+        Arrays.stream(nums).forEach(System.out::print);
+        System.out.println();
+        JikeCount.countingSort(nums,nums.length);
+        System.out.println("排序后:");
+        Arrays.stream(nums).forEach(System.out::print);
+    }
+}
